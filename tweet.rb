ee09b387a5ffac830e50c3a7268ceee59db67ffa
hiko1129 <wiz69gate4@hotmail.co.jp>
2016-06-19 09:02:48 +0900

    first commit
    create twitter bot

---
+++ tweet.rb
@@ -0,0 +1,13 @@
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = 'YOUR_CONSUMER_KEY'
+  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
+  config.access_token = 'YOUR_ACCESS_TOKEN'
+  config.access_token_secret = 'YOUR_ACCESS_SECRET'
+end
+
+# test
+puts 'input your tweet'
+input = gets.chomp
+client.update(input)
